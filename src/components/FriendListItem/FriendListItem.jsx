bd24f72d2fadd7ddd05461449b4b6aabfa02025e
Oleksandr Romanchuk <novator0511@gmail.com>
2022-12-28 20:29:47 +0200

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,19 +1,27 @@
 import PropTypes from 'prop-types';
 
 export default function FriendListItem(props) {
-    const {avatar, name, isOnline } = props;
+    const { friends } = props;
     
-    return <li className="item">
-                {isOnline
-                    ? <span className="status--online"></span>
-                    : <span className="status--offline"></span> }
-                <img className="avatar" src={avatar} alt="User avatar" width="48" />
-                <p className="name">{name}</p>
-            </li>
+    return <>
+                {friends.map(friend => (
+                    <li className="friend-list__item" key={friend.id}>
+                        {friend.isOnline
+                            ? <span className="status--online"></span>
+                            : <span className="status--offline"></span>}
+                        <img className="avatar" src={friend.avatar} alt="User avatar" width="48" />
+                        <p className="name">{friend.name}</p>
+                    </li>
+                    
+                ))}
+            </>;
 }
 
 FriendListItem.propTypes = {
-    avatar: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    isOnline: PropTypes.bool.isRequired,
+    friends: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        avatar: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        isOnline: PropTypes.bool.isRequired,
+    })),
 }
